d7fead533edad46adb3d7a8ff75b8615e5f0c696
Akshat Goyal <53331280+akshatgoyal276@users.noreply.github.com>
2025-03-10 17:31:56 +0530

    Initial Commit

---
+++ AdsDemoSwiftUI/AdsDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdsDemoSwiftUIApp.swift
+//  AdsDemoSwiftUI
+//
+//  Created by Adster on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AdsDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdsDemoSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdsDemoSwiftUI
+//
+//  Created by Adster on 10/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
