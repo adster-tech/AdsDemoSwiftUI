64ff98c88cad0df0d0cba2f3762bcd2cac3f61a8
Divya Aggarwal <34433176+divyaaggarwal14@users.noreply.github.com>
2025-11-13 09:41:14 +0530

    crash fix

--- MoneyMath/AppDelegate.swift
+++ MoneyMath/AppDelegate.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
